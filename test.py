f0f0728fa3940c742612921d478742150c913a07
Fadi Arafeh <fadi16@meet.mit.edu>
2015-02-22 20:00:46 +0200

    lol

---
+++ test.py
@@ -0,0 +1,14 @@
+import turtle
+screen = turtle.getscreen()
+screen.register_shape("mario2.gif")
+turtle.shape("mario2.gif")
+turtle.goto(500,50)
+
+screen = turtle.getscreen()
+screen.register_shape("tube.gif")
+turtle.shape("tube.gif")
+turtle.goto(320,30)
+
+
+
+turtle.mainloop()
