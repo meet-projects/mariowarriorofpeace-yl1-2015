a7c23663fc8854fe6c3e5268441c88e728dabe3d
Dina Jabr <dina16@meet.mit.edu>
2015-03-01 20:04:21 +0200

    nomsg

--- dinaclass.py
+++ dinaclass.py
@@ -2,11 +2,31 @@ screenMinX= -500
 screenMinY= -500
 screenMaxX= 500
 screenMaxY= 500
+import turtle
+screen= turtle.getscreen()
+screen.register_shape("goodmushroom.gif")
+
 class Good_mushrooms(Turtle):
-	def __init__(self,canvas,x,y,dx,dy,shape):
+	def __init__(self,canvas,x,y,dx,shape,radius):
 		RawTurtle.__init__(self,canvas)
 		self.penup()
 		self.goto(x,y)
 		self.dx = dx
-		self.dy = dy
+		self.radius=radius
 		self.shape("goodmushroom.gif")
+
+	def getDX(self):
+        		return self.dx
+    
+    def setDX(self,dx):
+        				self.dx =dx
+    
+    def moveleft(self):
+		 		x = self.xcor()
+		 	    self.goto(x-dx,y)
+    
+    def getRadius(self):
+        		return   self.radius
+ 
+
+ dina=Good_mushrooms(x=10,y=10,dx=7,radius=10)
--- test.py
+++ test.py
@@ -1,14 +1,9 @@
 import turtle
 screen = turtle.getscreen()
-screen.register_shape("mario2.gif")
-turtle.shape("mario2.gif")
+screen.register_shape("goodmushroom.gif")
+turtle.shape("goodmushroom.gif")
 turtle.goto(500,50)
-
-screen = turtle.getscreen()
-screen.register_shape("tube.gif")
-turtle.shape("tube.gif")
-turtle.goto(320,30)
-
+ 
 
 
 turtle.mainloop()
